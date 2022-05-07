da28ba3956a3338fb5e57493f3e17d74516d863a
Basudev Bharat Bhushan <bharatbhushan173@gmail.com>
2022-05-07 20:00:00 +0530

    Updating json files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,34 @@
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// module.exports = {
+//   solidity: "0.8.9",
+//   paths:{
+//     artifacts:"./frontend/src/artifacts"
+//   },
+//   networks:{
+// 	    harhat:{
+// 	  	  chainId:1337,
+//       },
+// 	  }
+// };
+
 module.exports = {
   solidity: "0.8.9",
-  paths:{
-    artifacts:"./frontend/src/artifacts"
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
   }
 };
